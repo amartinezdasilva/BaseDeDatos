5763744f4d11b45e8919e2da02ce39b9ea67f1bf
Aaron <Aaron@DESKTOP-BCLEH27>
2016-06-11 18:46:05 +0200

    commit subida

---
+++ src/basedatosprog/BaseDatosProg.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basedatosprog;
+
+
+
+/**
+ *
+ * @author Aaron
+ */
+public class BaseDatosProg {
+
+    
+    public static void main(String[] args)  {
+        Interfaz obxi = new Interfaz();
+        obxi.conectar();
+        obxi.setVisible(true);
+       
+    }
+    
+}
